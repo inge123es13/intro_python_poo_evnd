bb119f8ac0fdea38cd90c386fc00e23bad9f763d
inge123es13 <ingridestefanyp@gmail.com>
2026-05-12 21:07:20 -0600

    Tercer commit local

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
     print("Hello, Word")
     print("Hola de nuevo, Mundo")
+    
+    print("Suma de dos números")
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+    
+    
+    suma = num1 + num2
+
+    print("La suma es:", suma)
 
 if __name__ == "__main__":
     main()
+
